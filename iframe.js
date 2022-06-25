d6c363fe6912038debb1e55ef185fda0bec303c6
Jackson Horton <jacksonhorton1212@gmail.com>
2022-06-25 12:34:16 -0500

    Add files via upload

--- iframe.js
+++ iframe.js
@@ -9,22 +9,21 @@ function addElement() {
 
     // defines <div> as a variable; where HTML is added
     var node = document.getElementById('node-id');
-    node.innerHTML += `<div style="padding-left: 3rem;
-        padding - right: 3rem;
-        position: block;
-        width: 50 %;
-        height: auto;
-        margin - left: auto;
-        margin - right: auto; ">`;
-    node.innerHTML += `<div style="z-index: 1; position: block;" class="cloudimage-360" id="`+ model +`"
-        data-folder="https://raw.githubusercontent.com/trailersbypremier/360-images/master/` + model + `/"
-        data-filename-x="{index}.jpg" data-amount-x="8" data-play-once="true" data-autoplay="true" data-speed="30"
-        data-bottom-circle="true" data-hide-360-logo="false" data-stop-at-edges="false" data-pointer-zoom="none">
-    </div>`;
-    node.innerHTML += `<div style='background-color: green; position: relative; z-index: 0; width: 100%; height: auto; top: -1rem;'>
-            <img style='width: 40%; height: auto; position: relative; top: .5rem;'
+
+    var html = `<div class="threesixty">
+        <div style="z-index: 1; position: block;" class="cloudimage-360" id="` + model + `"
+            data-folder="https://raw.githubusercontent.com/trailersbypremier/360-images/master/` + model + `/"
+            data-filename-x="{index}.jpg" data-amount-x="8" data-play-once="true" data-autoplay="true" data-speed="30"
+            data-bottom-circle="true" data-hide-360-logo="false" data-stop-at-edges="false" data-pointer-zoom="none">
+        </div>
+        <img class="noselector" src="https://raw.githubusercontent.com/trailersbypremier/360-images/master/360.png"
+            style="width:10%; z-index: 2; transform: translate(0%, -100%); position: absolute;">
+        <div class="threesixty-title">
+            <img class="noselector"
                 src="https://raw.githubusercontent.com/trailersbypremier/360-images/master/tbp-logo.webp">
-            <h1 style="position: relative;color: white; float: right; font-size: calc(2vw + 2vh + 1vmin); font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">T612</h1>
-        </div>`;
-    node.innerHTML += '</div>';
+            <h1>` + model + `</h1>
+        </div>
+    </div>`;
+
+    node.innerHTML = html;
 }
