a5e0894815a6f6ccc042f17a635fcf28ebe31dd5
trailersbypremier <jack.hortontbp@gmail.com>
2022-06-28 14:03:56 -0500

    Made iframe horizontal.

--- iframe.js
+++ iframe.js
@@ -10,21 +10,21 @@ function addElement() {
     // defines <div> as a variable; where HTML is added
     var node = document.getElementById('node-id');
 
-    var html = `<div class="threesixty">
-        <div style="z-index: 1; position: block;" class="cloudimage-360" id="` + model + `"
-            data-folder="https://raw.githubusercontent.com/trailersbypremier/360-images/master/` + model + `/"
-            data-filename-x="{index}.jpg" data-amount-x="8" data-play-once="true" data-autoplay="true" data-speed="30"
-            data-bottom-circle="true" data-hide-360-logo="false" data-stop-at-edges="false" data-pointer-zoom="none">
-        </div>
-        <img class="noselector" src="https://raw.githubusercontent.com/trailersbypremier/360-images/master/360.png"
-            style="width:10%; z-index: 2; transform: translate(0%, -100%); position: absolute;">
-        
-        <div class="threesixty-title">
-            <img class="noselector"
-                src="https://raw.githubusercontent.com/trailersbypremier/360-images/master/tbp-logo.webp">
-            <h1>` + model + `</h1>
-        </div>
-    </div>`;
+    var html = `<div class="threesixty" style="display: flex;">
+                    <div style="max-width: 75%; z-index: 1; position: block;" class="cloudimage-360" id="` + model + `"
+                        data-folder="https://raw.githubusercontent.com/trailersbypremier/360-images/master/` + model + `/"
+                        data-filename-x="{index}.jpg" data-amount-x="8" data-play-once="true" data-autoplay="true" data-speed="30"
+                        data-bottom-circle="true" data-hide-360-logo="false" data-stop-at-edges="false" data-pointer-zoom="none">
+                    </div>
+                    <img class="noselector" src="https://raw.githubusercontent.com/trailersbypremier/360-images/master/360.png"
+                        style="width:10%; z-index: 2; position: absolute; padding: 1rem;">
+
+                        <div class="threesixty-title">
+                            <h1>` + model + `</h1>
+                            <img class="noselector"
+                                src="https://raw.githubusercontent.com/trailersbypremier/360-images/master/tbp-logo.webp">
+                        </div>
+                </div>`;
 
     node.innerHTML = html;
 }
